ac3718144e285d1f69653c0adf6cf08a4dd29083
Paul Florence <perso@florencepaul.com>
2019-03-08 17:12:09 +0100

    Integrate LEX with CMake

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include "lex.yy.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage : %s <file>",argv[0]);
+        exit(1);
+    }
+    yyin = fopen(argv[1], "r");
+    yylex();
+    fclose(yyin);
     return 0;
 }
