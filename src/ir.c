b4cccfbc6a5b36abed053e03174b8e340eec851d
Paul Florence <perso@florencepaul.com>
2019-03-20 17:42:35 +0100

    Add Intermediate Representation (IR) scaffold

---
+++ src/ir.c
@@ -0,0 +1,26 @@
+//
+// Created by paul on 20/03/19.
+//
+
+#include "ir.h"
+
+struct ir_body {
+    ir_body* next;
+    ir_ins* instr;
+};
+
+struct ir_ins {
+
+    union {
+        struct ins {
+            int op;
+            int p1;
+            int p2;
+        };
+        struct _if {
+            ir_body* if_cond;
+            ir_body* if_body;
+        }
+    };
+
+};
---
+++ src/ir.h
@@ -0,0 +1,12 @@
+//
+// Created by paul on 20/03/19.
+//
+
+#ifndef AUL_IR_H
+#define AUL_IR_H
+
+
+typedef struct ir_body ir_body;
+typedef struct ir_ins ir_ins;
+
+#endif //AUL_IR_H
