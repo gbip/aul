c8cbccf2bc104b5fa024b0e2e0401af591ed8f4e
Paul Florence <perso@florencepaul.com>
2019-03-21 11:25:14 +0100

    Fix cmocka integration

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ You need to have [`cmocka`](https://cmocka.org) installed to run unit tests. Sin
 ```
 sudo apt install cmocka
 ```
+
+To use `cmocka` take a look at [their documentation](https://api.cmocka.org/group__cmocka__asserts.html).
--- src/aul_utils.h
+++ src/aul_utils.h
@@ -9,15 +9,20 @@
 
 
 #ifdef UNIT_TESTING
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
-extern void* test_malloc(const size_t size, const char* file, const int line);
+
+
+extern void* _test_malloc(const size_t size, const char* file, const int line);
+extern void* _test_calloc(const size_t number_of_elements, const size_t size,
+                          const char* file, const int line);
+extern void _test_free(void* const ptr, const char* file, const int line);
+
 #define malloc(size) _test_malloc(size, __FILE__, __LINE__)
-extern void* _test_free(void * ptr);
-#define free(size) _test_free(size)
+#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
+#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
+
+
 #else
-#include "stdlib.h"
+#include <stdlib.h>
 #endif
 
 
